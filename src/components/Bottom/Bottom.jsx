a2e57f86011db52c21e563fa5a31010e702d4f0c
Rayhan Islam <raihanislamshagor2@gmail.com>
2022-08-14 14:11:29 +0600

    add redux devtool in react app

--- src/components/Bottom/Bottom.jsx
+++ src/components/Bottom/Bottom.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import styles from '../../styles/Bottom.module.css';
-import { modules } from '../../utils/Modules';
-import BackButton from '../buttons/BackButton';
-import PrimaryButton from '../buttons/PrimaryButton';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import styles from "../../styles/Bottom.module.css";
+import { modules } from "../../utils/Modules";
+import BackButton from "../buttons/BackButton";
+import PrimaryButton from "../buttons/PrimaryButton";
 
 const Bottom = ({ handleSubmit, data }) => {
   const locations = useLocation();
   const currentPath = locations.pathname;
   const index = modules.findIndex((module) => module.path === currentPath);
   const nextPath = modules[index + 1].path;
+
   return (
     <div className={styles.btnContainer}>
-      <BackButton text='Back' url='/' />
+      <BackButton text="Back" url="/" />
       <PrimaryButton
         handleSubmit={handleSubmit}
         data={data}
-        text='Save & Continue'
+        text="Save & Continue"
         url={nextPath}
       />
     </div>
