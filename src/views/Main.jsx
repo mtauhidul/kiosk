4616b0de32567af4543bf8c19a531eb30504867d
Rayhan0Islam0Shagor <raihanislamshagor2@gmail.com>
2022-09-24 12:10:38 +0600

    sidebar added

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Button } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
@@ -11,17 +12,102 @@ import ListItemText from "@mui/material/ListItemText";
 import Toolbar from "@mui/material/Toolbar";
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/images/logo.svg";
+import useWindowSize from "../hooks/useWindowSize";
 import styles from "../styles/Main.module.css";
 import { listItems } from "../utils/Menus";
+
+import { AiOutlineMenuUnfold } from "react-icons/ai";
 
 const drawerWidth = 240;
 
 export default function Main(props) {
   const navigate = useNavigate();
+  const { width } = useWindowSize();
 
-  const handleClick = (url, index) => {
-    navigate(url);
+  const [state, setState] = React.useState({
+    left: false,
+    size: false,
+  });
+
+  React.useEffect(() => {
+    if (width < 1200) {
+      setState({
+        left: false,
+        size: true,
+      });
+    } else {
+      setState({
+        left: false,
+        size: false,
+      });
+    }
+  }, [width]);
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
   };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List
+        sx={{
+          position: "sticky",
+          top: 0,
+          backgroundColor: "#fff",
+          zIndex: 999,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          onClick={() => navigate("/")}
+          className={styles.drawerLogo}
+          src={Logo}
+          alt="Logo"
+        />
+      </List>
+
+      <List>
+        {listItems.map((item, index) => (
+          <ListItem button key={index} className={styles.listItem}>
+            {props.title.includes(item.item) ? (
+              <ListItemIcon>
+                <img
+                  style={{
+                    filter:
+                      "invert(49%) sepia(35%) saturate(6495%) hue-rotate(220deg) brightness(99%) contrast(93%)",
+                  }}
+                  src={item.icon}
+                  alt="Menu Icon"
+                />
+              </ListItemIcon>
+            ) : (
+              <ListItemIcon>
+                <img src={item.icon} alt="Menu Icon" />
+              </ListItemIcon>
+            )}
+            {props.title.includes(item.item) ? (
+              <ListItemText primary={item.item} sx={{ color: "#6A6EF4" }} />
+            ) : (
+              <ListItemText primary={item.item} />
+            )}
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <Box className={styles.mainContainer} sx={{ display: "flex" }}>
@@ -30,78 +116,119 @@ export default function Main(props) {
         className={styles.header}
         position="fixed"
         sx={{
-          width: `calc(100% - ${drawerWidth}px)`,
-          ml: `${drawerWidth}px`,
+          width: state.size ? "100%" : `calc(100% - ${drawerWidth}px)`,
+          ml: state.size ? 0 : `${drawerWidth}px`,
         }}
       >
         <Toolbar className={styles.contentHeader}>
+          <div>
+            {["left"].map((anchor) => (
+              <React.Fragment key={anchor}>
+                {state.size && (
+                  <Button onClick={toggleDrawer(anchor, true)} size="large">
+                    {state.left ? (
+                      <AiOutlineMenuUnfold
+                        fontSize="30px"
+                        style={{ color: "#000" }}
+                      />
+                    ) : (
+                      <AiOutlineMenuUnfold
+                        fontSize="30px"
+                        style={{ color: "#000" }}
+                      />
+                    )}
+                  </Button>
+                )}
+
+                <Drawer
+                  anchor={anchor}
+                  open={state[anchor]}
+                  onClose={toggleDrawer(anchor, false)}
+                >
+                  {list(anchor)}
+                </Drawer>
+              </React.Fragment>
+            ))}
+          </div>
+
           <h2 className="header2">{props.title}</h2>
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={styles.sideBar}
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
+
+      {!state.size && (
+        <Drawer
+          className={styles.sideBar}
+          sx={{
             width: drawerWidth,
-            boxSizing: "border-box",
-            borderRight: "none",
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar>
-          <img
-            onClick={() => navigate("/")}
-            className={styles.drawerLogo}
-            src={Logo}
-            alt="Logo"
-          />
-        </Toolbar>
-        <List>
-          {listItems.map((item, index) => (
-            <ListItem
-              // onClick={() => handleClick(item.url, index)}
-              button
-              key={index}
-              className={styles.listItem}
-            >
-              {props.title.includes(item.item) ? (
-                <ListItemIcon>
-                  <img
-                    style={{
-                      filter:
-                        "invert(49%) sepia(35%) saturate(6495%) hue-rotate(220deg) brightness(99%) contrast(93%)",
-                    }}
-                    src={item.icon}
-                    alt="Menu Icon"
-                  />
-                </ListItemIcon>
-              ) : (
-                <ListItemIcon>
-                  <img src={item.icon} alt="Menu Icon" />
-                </ListItemIcon>
-              )}
-              {props.title.includes(item.item) ? (
-                <ListItemText primary={item.item} sx={{ color: "#6A6EF4" }} />
-              ) : (
-                <ListItemText primary={item.item} />
-              )}
-            </ListItem>
-          ))}
-        </List>
-        <Button
-          onClick={() => navigate("/")}
-          variant="contained"
-          size="medium"
-          className="backHomeButton"
-          startIcon={<ArrowBackIcon />}
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+              borderRight: "none",
+            },
+          }}
+          variant="permanent"
+          anchor="left"
         >
-          Back to Home
-        </Button>
-      </Drawer>
+          <Toolbar
+            sx={{
+              position: "sticky",
+              top: 0,
+              backgroundColor: "#fff",
+              zIndex: 999,
+            }}
+          >
+            <img
+              onClick={() => navigate("/")}
+              className={styles.drawerLogo}
+              src={Logo}
+              alt="Logo"
+            />
+          </Toolbar>
+          <List>
+            {listItems.map((item, index) => (
+              <ListItem button key={index} className={styles.listItem}>
+                {props.title.includes(item.item) ? (
+                  <ListItemIcon>
+                    <img
+                      style={{
+                        filter:
+                          "invert(49%) sepia(35%) saturate(6495%) hue-rotate(220deg) brightness(99%) contrast(93%)",
+                      }}
+                      src={item.icon}
+                      alt="Menu Icon"
+                    />
+                  </ListItemIcon>
+                ) : (
+                  <ListItemIcon>
+                    <img src={item.icon} alt="Menu Icon" />
+                  </ListItemIcon>
+                )}
+                {props.title.includes(item.item) ? (
+                  <ListItemText primary={item.item} sx={{ color: "#6A6EF4" }} />
+                ) : (
+                  <ListItemText primary={item.item} />
+                )}
+              </ListItem>
+            ))}
+          </List>
+
+          <Button
+            onClick={() => navigate("/")}
+            variant="contained"
+            size="medium"
+            className="backHomeButton"
+            startIcon={<ArrowBackIcon />}
+            sx={{
+              position: "sticky",
+              bottom: "0",
+            }}
+          >
+            Back to Home
+          </Button>
+        </Drawer>
+      )}
+
       <Box
         className={styles.body}
         component="main"
